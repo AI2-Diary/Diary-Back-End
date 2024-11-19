9565b8d53eeabb969c5f7a37dfe9c727ef9d2a6f
강승우 <phonil@naver.com>
2024-11-19 12:18:59 +0900

    init project

---
+++ ai2_diary/src/test/java/com/example/ai2_diary/Ai2DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai2_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ai2DiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
